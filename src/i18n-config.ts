f47afec98d5e875656f1de8a87584089162a7051
Vilius Valinskis <vilius.valinsk@gmail.com>
2025-04-13 19:14:47 +0300

    latvian

--- src/i18n-config.ts
+++ src/i18n-config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
     defaultLocale: "lt",
-    locales: ["lt", "en"],
+    locales: ["lt", "en", "lv"],
   } as const;
   
 export type Locale = (typeof i18n)["locales"][number];
