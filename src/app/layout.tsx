1568d772c186bc0b552649b180caa1325ba2ac3f
Vilius Valinskis <vilius.valinsk@gmail.com>
2025-04-13 17:06:17 +0300

    ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
+      <GoogleAnalytics gaId="GTM-TW3Q5DLK" />
     </html>
   );
 }
