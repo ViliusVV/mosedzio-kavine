15b20da2c2246c8a44067260634f20b26714dad5
Vilius Valinskis <vilius.valinsk@gmail.com>
2025-04-13 14:40:49 +0300

    responsive

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -3,16 +3,16 @@ import PageTemplate from "@/components/page-template";
 
 export default function ContactsPage() {
     return <PageTemplate>
-        <div className="flex flex-col gap-[32px] row-start-2">
+        <div className="flex flex-col gap-[32px] row-start-2 w-full">
             <h1 className="text-4xl">Kontaktai</h1>
-            <div>
+            <div className="max-w-sreen-sm">
                 <p className="text-xl">Tel. nr.: +370 645 44112</p>
-                <p className="text-xl">Adresas: Skuodo r., Mosėdis, Kęstučio g. 6a</p>
+                <p className="text-xl ">Skuodo r., Mosėdis, Kęstučio g. 6a</p>
             </div>
-            <div>
+            <div className="relative h-[400px] md:h-[500px]">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2221.457358443992!2d21.573653311940706!3d56.16648635994115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e54118263eea29%3A0x34818a0c2c94bdb!2zTW9zxJdkxb5pbyBrYXZpbsSX!5e0!3m2!1slt!2slt!4v1744536419247!5m2!1slt!2slt" 
-                    width="600" 
-                    height="450" 
+                    width="100%"
+                    height="100%"
                     style={{"border":"0"}} 
                     allowFullScreen={false} 
                     loading="lazy" 
--- src/components/page-template.tsx
+++ src/components/page-template.tsx
@@ -6,7 +6,7 @@ export default function PageTemplate(props: {
   return <div className="">
       <Topbar/>
       <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen md:p-8 p-2 gap-8">
-        <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        <main className="flex flex-col gap-[32px] w-full lg:w-[900px]  row-start-2 items-center">
           {props.children}
         </main>
         <footer className="row-start-3 flex flex-wrap items-center justify-center">
