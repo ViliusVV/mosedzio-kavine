e52a3db92b597d3b2dc9c3c16028592c2137a5de
Vilius Valinskis <vilius.valinsk@gmail.com>
2025-04-13 19:16:37 +0300

    build fix

--- src/utils/lang.ts
+++ src/utils/lang.ts
@@ -1,4 +1,5 @@
 import data from "@/data/i18n.json"
+import { Locale } from "@/i18n-config"
 
 type LangData = {
     lt: {
@@ -6,13 +7,16 @@ type LangData = {
     },
     en: {
         [key: string]: string
+    },
+    lv: {
+        [key: string]: string
     }
 }
 
 const langData = data as unknown as LangData
 
-export type StringKey = keyof typeof data["lt"] | keyof typeof data["en"]
+export type StringKey = keyof typeof data["lt"] | keyof typeof data["en"] | keyof typeof data["lv"]
 
-export function T(lang: "lt" | "en", key: StringKey): string {
+export function T(lang: Locale, key: StringKey): string {
     return langData[lang][key] || langData[lang]["lt"] || key as string
 }
